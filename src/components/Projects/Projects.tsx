ca36bd498a9c2e5451f00bd68a70094d673c3fd1
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 19:14:38 +0300

    Update Projects.tsx

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import styles from './Projects.module.css';
 import containerStyle from './../../common/styles/Container.module.css';
 import { Project } from './Project/Project';
+import { Title } from '../../common/components/title/Title';
 
 export const Projects: React.FC = () => {
    return <div className={styles.projectsBlock}>
       <div className={`${containerStyle.container} ${styles.projectsContainer}`}>
-         <h2 className={styles.title}>My Projects</h2>
+         <Title text={'My Projects'} />
          <div className={styles.projects}>
-            <Project projectName='TodoList' 
-               projectDir='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, facere!'/>
+            <Project projectName='TodoList'
+               projectDir='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, facere!' />
             <Project projectName='Social Network'
-               projectDir='Lorem ipsum dolor sit amet consectetur adipisicing elit. A, modi?'/>
+               projectDir='Lorem ipsum dolor sit amet consectetur adipisicing elit. A, modi?Lorem ipsum dolor sit amet consectetur adipisicing elit. A, modi?Lorem ipsum dolor sit amet consectetur adipisicing elit. A, modi?Lorem ipsum dolor sit amet consectetur adipisicing elit. A, modi?' />
          </div>
       </div>
    </div>
-} 
+}
