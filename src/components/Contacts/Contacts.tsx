8ba459b15fc5f971ccea6e67eff0217927a5991b
Андрей Ашмянец <andreka6033@gmail.com>
2022-08-15 19:14:14 +0300

    Update Contacts.tsx

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Contacts.module.css';
 import containerStyle from './../../common/styles/Container.module.css';
+import { Title } from '../../common/components/title/Title';
 
 export const Contacts: React.FC = () => {
    return <div className={styles.contactsBlock}>
       <div className={`${containerStyle.container} ${styles.contactsContainer}`}>
-         <h2 className={styles.title}>Contacts</h2>
+         <Title text={'Contacts'} />
          <form className={styles.form} action="">
             <input />
             <input />
