25fa376a8afa104a33b9e37921b540ddbc3aeb33
Андрей Ашмянец <andreka6033@gmail.com>
2022-10-13 23:13:02 +0300

    refactor

---
+++ src/common/components/particles/Particle.tsx
@@ -0,0 +1,93 @@
+import React, {useCallback} from 'react';
+import Particles from 'react-particles';
+import type {Container, Engine} from 'tsparticles-engine';
+import {loadFull} from 'tsparticles';
+import styles from './Particle.module.scss';
+
+export const Particle: React.FC = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        console.log(engine);
+
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container: Container | undefined) => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <Particles
+            id="tsparticles"
+            className={styles.particles}
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 3,
+                        },
+                        repulse: {
+                            distance: 100,
+                            duration: 0.9,
+                        },
+                    },
+                },
+                particles: {
+                    links: {
+                        color: '#fff',
+                        distance: 150,
+                        enable: true,
+                        opacity: 1,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 3,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.3,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import styles from './Main.module.scss';
 import containerStyle from './../../common/styles/Container.module.css';
+import {Particle} from "../../common/components/particles/Particle";
 
 export const Main: React.FC = () => {
     return <div className={styles.mainBlock}>
+        <Particle/>
         <div className={`${containerStyle.container} ${styles.mainContainer}`}>
             <div className={styles.text}>
                 <span>Hi There</span>
-                <h1>I am <span>Andrew Ashmianets</span></h1>
+                <h1>I am <span className={styles.name}>Andrew Ashmianets</span></h1>
                 <p>Frontend Developer</p>
             </div>
             <div className={styles.photo}>
+                <div className={styles.image}>
 
+                </div>
             </div>
         </div>
     </div>
