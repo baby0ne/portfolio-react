c2fbacae5d7ebfff18669df14093ec24d5d2826d
Андрей Ашмянец <andreka6033@gmail.com>
2022-10-16 13:27:33 +0300

    refactor

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './Main.module.scss';
-import containerStyle from './../../common/styles/Container.module.css';
-import {Particle} from "../../common/components/particles/Particle";
-import {Fade} from "react-awesome-reveal";
+import containerStyle from '../../common/styles/Container.module.css';
+import {Particle} from '../../common/components/particles/Particle';
+import {Fade} from 'react-awesome-reveal';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from "react-parallax-tilt";
 
 export const Main: React.FC = () => {
     return <div className={styles.mainBlock} id={'main'}>
@@ -12,13 +14,17 @@ export const Main: React.FC = () => {
                 <div className={styles.text}>
                     <span>Hi There</span>
                     <h1>I am <span className={styles.name}>Andrew Ashmianets</span></h1>
-                    <p>Frontend Developer</p>
+                    <ReactTypingEffect
+                        className={styles.typingText}
+                        text={['Frontend Developer.', 'React Developer.']}
+                        speed={100}
+                    />
                 </div>
-                <div className={styles.photo}>
-                    <div className={styles.image}>
-
+                <Tilt tiltMaxAngleX={8} tiltMaxAngleY={8}>
+                    <div className={styles.photo}>
+                        <div className={styles.image}></div>
                     </div>
-                </div>
+                </Tilt>
             </Fade>
         </div>
     </div>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Navigation.module.scss';
-
+import {Link} from "react-scroll";
 
 export const Navigation: React.FC = () => {
-   return <div className={styles.navigation}>
-      <a href='#main'>Main</a>
-      <a href='#skills'>Skills</a>
-      <a href='#projects'>Projects</a>
-      <a href='#contacts'>Contacts</a>
-   </div>
+    return <div className={styles.navigation}>
+        <Link to="main" smooth={true} offset={-80}>Main</Link>
+        <Link to="skills" smooth={true} offset={-80}>Skills</Link>
+        <Link to="projects" smooth={true} offset={-80}>Projects</Link>
+        <Link to="contacts" smooth={true} offset={-80}>Contacts</Link>
+    </div>
 }
