49dec85697555fada03ca4a5855e1ee8ebd3ba83
Андрей Ашмянец <andreka6033@gmail.com>
2022-10-22 22:19:38 +0300

    finish

--- src/common/components/particles/Particle.tsx
+++ src/common/components/particles/Particle.tsx
@@ -1,21 +1,15 @@
 import React, {useCallback} from 'react';
 import Particles from 'react-particles';
-import type {Container, Engine} from 'tsparticles-engine';
+import type {Engine} from 'tsparticles-engine';
 import {loadFull} from 'tsparticles';
 import styles from './Particle.module.scss';
 
 export const Particle: React.FC = () => {
     const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-
         // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
         // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
         // starting from v2 you can add only the features you need reducing the bundle size
         await loadFull(engine);
-    }, []);
-
-    const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
     }, []);
 
     return (
@@ -23,7 +17,6 @@ export const Particle: React.FC = () => {
             id="tsparticles"
             className={styles.particles}
             init={particlesInit}
-            loaded={particlesLoaded}
             options={{
                 fpsLimit: 120,
                 fullScreen: {
--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, {KeyboardEvent} from 'react';
 import styles from './Contacts.module.scss';
 import containerStyle from './../../common/styles/Container.module.css';
 import {Title} from '../../common/components/title/Title';
 import {Fade} from 'react-awesome-reveal';
+import {useFormik} from "formik";
+import axios from "axios";
+import Swal from 'sweetalert2';
 
 export const Contacts: React.FC = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+        },
+        onSubmit: values => {
+            axios.post('https://gmail-smtp-andreka.herokuapp.com/sendMessage', values)
+                .then(res => {
+                    Swal.fire(
+                        'Thanks for the feedback!',
+                        'You will get an answer soon!',
+                        'success'
+                    )
+                })
+                .catch(err => {
+                    Swal.fire({
+                        title: 'Oops...Something went wrong.',
+                        text: `Error: ${err.message}`,
+                        icon: 'error',
+                    })
+                })
+
+            formik.resetForm();
+        },
+    });
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            formik.handleSubmit()
+        }
+    }
+
     return <div className={styles.contactsBlock} id={'contacts'}>
         <div className={`${containerStyle.container} ${styles.contactsContainer}`}>
             <Title text={'Contacts'}/>
-            <form className={styles.form} action="">
+            <form className={styles.form} onSubmit={formik.handleSubmit}>
                 <Fade direction={'up'} duration={700} triggerOnce={true}>
                     <input className={styles.formControl}
                            placeholder={'Name'}
+                           {...formik.getFieldProps('name')}
+                           onKeyPress={onKeyPressHandler}
                            required/>
                     <input className={styles.formControl}
                            placeholder={'E-Mail'}
+                           {...formik.getFieldProps('email')}
+                           onKeyPress={onKeyPressHandler}
                            required/>
                     <textarea className={styles.formControl}
                               placeholder={'Your message'}
+                              {...formik.getFieldProps('message')}
+                              onKeyPress={onKeyPressHandler}
                               required/>
-                    <button className={styles.sendButton}>Send</button>
+                    <button className={styles.sendButton} type={'submit'}>Send</button>
                 </Fade>
             </form>
-
         </div>
     </div>
-
 }
