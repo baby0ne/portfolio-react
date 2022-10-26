6c8eb1653665aabfe1a894523e504325f2406c77
Андрей Ашмянец <andreka6033@gmail.com>
2022-10-26 22:13:18 +0300

    finish v.2

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -17,7 +17,7 @@ export const Contacts: React.FC = () => {
             message: '',
         },
         onSubmit: values => {
-            axios.post('https://localhost:8080/email/test', values)
+            axios.post('http://localhost:8080/email/test', values)
                 .then(res => {
                     Swal.fire(
                         'Thanks for the feedback!',
